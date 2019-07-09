e7f93288dcf0bdd1b079da4e204ba25e4f12d380
FatmaOueslati <fatma.oueslati01@gmail.com>
2019-07-09 13:55:44 +0100

    interfaces

--- frontPFE/src/app/form/form.routing.ts
+++ frontPFE/src/app/form/form.routing.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 
-import { BasicComponent } from './basic/basic.component';
-import { MasksComponent } from './masks/masks.component';
+
 import { EditorComponent } from './editor/editor.component';
 import { ValidationComponent } from './validation/validation.component';
 import { UploadComponent } from './upload/upload.component';
@@ -10,19 +9,7 @@ import { UploadComponent } from './upload/upload.component';
 export const FormRoutes: Routes = [
   {
     path: '',
-    children: [{
-      path: 'basic',
-      component: BasicComponent,
-      data: {
-        heading: 'Basic forms'
-      }
-    }, {
-      path: 'masks',
-      component: MasksComponent,
-      data: {
-        heading: 'Masks'
-      }
-    }, {
+    children: [  {
       path: 'editor',
       component: EditorComponent,
       data: {
--- frontPFE/src/app/fullcalendar/fullcalendar.component.ts
+++ frontPFE/src/app/fullcalendar/fullcalendar.component.ts
@@ -70,23 +70,7 @@ export class FullcalendarComponent {
 
   refresh: Subject<any> = new Subject();
 
-  events: CalendarEvent[] = [{
-    start: subDays(startOfDay(new Date()), 1),
-    end: addDays(new Date(), 1),
-    title: 'A 3 day event',
-    color: colors.red,
-    actions: this.actions
-  }, {
-    start: startOfDay(new Date()),
-    title: 'An event with no end date',
-    color: colors.yellow,
-    actions: this.actions
-  }, {
-    start: subDays(endOfMonth(new Date()), 3),
-    end: addDays(endOfMonth(new Date()), 3),
-    title: 'A long event that spans 2 months',
-    color: colors.blue
-  }, {
+  events: CalendarEvent[] = [  {
     start: addHours(startOfDay(new Date()), 2),
     end: new Date(),
     title: 'A draggable and resizable event',
